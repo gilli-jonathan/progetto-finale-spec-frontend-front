c4b7b9f1fa1ea9d9391a2d1e30d30e1e9c116dac
gilli-jonathan <gilli.jonathan97@gmail.com>
2026-04-17 11:14:54 +0200

    add functionality search bar

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,19 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GameContext } from "../context/GlobalContext";
 
 export default function GameList() {
 
-    const GamesList = useContext(GameContext)
-    const games = GamesList
-    console.log(games);
+    //prendo la lista di tutti i giochi + la stringa per il filtro di ricerca
+    const { games, searchQuery } = useContext(GameContext)
 
+    //filtrami i contenuti in cui game.title contiene il valore di searchQuery, tutto lowerCase
+    const gamesList = games.filter(game => game.title.toLowerCase().includes(searchQuery.toLowerCase()))
 
     return (
 
         <>
             <h3>elenco dei giochi</h3>
             {
-                games.map((game => (
+                gamesList.map((game => (
                     <h5 key={game.id}>{game.title}</h5>
                 )))
             }
